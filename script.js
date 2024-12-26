447c5f96ee498de9a9ee8549ea69f0c5269d47b7
TamannaSharma987 <sharmat09082007@gmail.com>
2024-12-26 10:37:32 +0530

    c

--- script.js
+++ script.js
@@ -35,3 +35,21 @@ window.onmousemove = (e) =>{
         cursor2.classList.remove('active');
     }
 })
+
+
+
+function gotowhatsapp() {
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var sub = document.getElementById("sub").value;
+    var msg = document.getElementById("msg").value;
+
+    var url = "https://wa.me/+919426671316?text=" 
+    + "Name: " + name + "%0a"
+    + "Email: " + email + "%0a"
+    + "Subject: " + sub + "%0a"
+    + "Message: " + msg;
+
+    window.open(url).focus();
+}
